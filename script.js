5f66474459a2c209faf725a93aa1c94beabc5109
Wojciech <wojciech.wantuch@gmail.com>
2022-01-14 21:15:59 +0100

    feat: convert time from MM into H and MM

--- script.js
+++ script.js
@@ -3,14 +3,33 @@ const apiAddress = 'https://todo-api.coderslab.pl';
 
 
 function apiListTasks() {
-    return fetch(apiAddress + '/api/tasks', { headers: { Authorization: apiKey }})
+    return fetch(apiAddress + '/api/tasks',
+                 { headers: { Authorization: apiKey }})
         .then(function(response) {
             if(!response.ok) {
                 alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
             }
-            return response.json();
+            return response.json()
         })
 }
+function apiListSubtasks(taskId) {
+    return fetch(apiAddress + '/api/tasks/' + taskId + '/operations',
+                 { headers: { Authorization: apiKey }})
+        .then(function(response) {
+            if(!response.ok) {
+                alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
+            }
+            return response.json()
+        })
+}
+
+function convertMinutesIntoHHMM(timeInMinutes) {
+    let hours = Math.floor(timeInMinutes / 60)
+    let minutes = timeInMinutes % 60
+    
+    return hours + 'h ' + minutes + 'm'
+}
+
 function renderTask(taskId, title, description, status) {
     const taskSection = document.createElement('section')
         taskSection.className = 'card mt-5 shadow-sm'
@@ -47,70 +66,75 @@ function renderTask(taskId, title, description, status) {
                     deleteTaskButton.innerText = 'Delete'
                     headerRightDiv.appendChild(deleteTaskButton)
 
-        const subtasksUl = document.createElement('ul')     
-            subtasksUl.className = 'list-group list-group-flush'
-            taskSection.appendChild(subtasksUl)
+            const subtasksUl = document.createElement('ul')     
+                subtasksUl.className = 'list-group list-group-flush'
+                taskSection.appendChild(subtasksUl)
 
-            const subtaskLi = document.createElement('li')
-                subtaskLi.className = 'list-group-item d-flex justify-content-between align-items-center'
-                subtasksUl.appendChild(subtaskLi)
-
-                const subtaskLeftDiv = document.createElement('div')
-                    subtaskLeftDiv.innerText = '___'
-                    subtaskLi.appendChild(subtaskLeftDiv)
-
-                    const subtaskTimeSpan = document.createElement('span')
-                        subtaskTimeSpan.className = 'badge badge-success badge-pill ml-2'
-                        subtaskTimeSpan.innerText = '___'
-                        subtaskLeftDiv.appendChild(subtaskTimeSpan)
-
-                const subtaskRightDiv = document.createElement('div')
-                    subtaskLi.appendChild(subtaskRightDiv)
-
-                    const plus15MinButton = document.createElement('button')
-                        plus15MinButton.className = 'btn btn-outline-success btn-sm mr-2'
-                        plus15MinButton.innerText = '+15m'
-                        subtaskRightDiv.appendChild(plus15MinButton)
-
-                    const plus1HButton = document.createElement('button')
-                        plus1HButton.className = 'btn btn-outline-success btn-sm mr-2'
-                        plus1HButton.innerText = '+1h'
-                        subtaskRightDiv.appendChild(plus1HButton)
-
-                    const deleteSubtaskButton = document.createElement('button')
-                        deleteSubtaskButton.className = 'btn btn-outline-danger btn-sm'
-                        deleteSubtaskButton.innerText = 'Delete'
-                        subtaskRightDiv.appendChild(deleteSubtaskButton)
-
-        const addSubtaskDiv = document.createElement('div')
-            addSubtaskDiv.className = 'card-body'
-            taskSection.appendChild(addSubtaskDiv)
-
-            const addSubtaskDivForm = document.createElement('form')
-                addSubtaskDiv.appendChild(addSubtaskDivForm)
-
-                const addSubtaskDivFormDiv = document.createElement('div')
-                    addSubtaskDivFormDiv.className = 'input-group'
-                    addSubtaskDivForm.appendChild(addSubtaskDivFormDiv)
-
-                    const addSubtaskDivFormDivInput = document.createElement('input')
-                        addSubtaskDivFormDivInput.type = 'text'
-                        addSubtaskDivFormDivInput.placeholder = 'Operation description'
-                        addSubtaskDivFormDivInput.className = 'form-control'
-                        addSubtaskDivFormDivInput.minlenght = '5'
-                        addSubtaskDivFormDiv.appendChild(addSubtaskDivFormDivInput)
-                    
-                    const addSubtaskDivFormDivDiv = document.createElement('div')
-                        addSubtaskDivFormDivDiv.className = 'input-group-append'
-                        addSubtaskDivFormDiv.appendChild(addSubtaskDivFormDivDiv)
-
-                        const addSubtaskDivFormDivDivButton = document.createElement('button')
-                            addSubtaskDivFormDivDivButton.className = 'btn btn-info'
-                            addSubtaskDivFormDivDivButton.innerText = 'Add'
-                            addSubtaskDivFormDivDiv.appendChild(addSubtaskDivFormDivDivButton)
-
-}
+                apiListSubtasks(taskId).then(function(response) {
+                    response.data.forEach((operation) => {
+                        renderSubtask(subtasksUl, operation.id, status, operation.description, operation.timeSpent)
+                    });
+                })
+            
+            const addSubtaskDiv = document.createElement('div')
+                addSubtaskDiv.className = 'card-body'
+                taskSection.parentElement.appendChild(addSubtaskDiv)
   
+                const addSubtaskDivForm = document.createElement('form')
+                    addSubtaskDiv.appendChild(addSubtaskDivForm)
+            
+                    const addSubtaskDivFormDiv = document.createElement('div')
+                        addSubtaskDivFormDiv.className = 'input-group'
+                        addSubtaskDivForm.appendChild(addSubtaskDivFormDiv)
+            
+                        const addSubtaskDivFormDivInput = document.createElement('input')
+                            addSubtaskDivFormDivInput.type = 'text'
+                            addSubtaskDivFormDivInput.placeholder = 'Subtask description'
+                            addSubtaskDivFormDivInput.className = 'form-control'
+                            addSubtaskDivFormDivInput.minlenght = '5'
+                            addSubtaskDivFormDiv.appendChild(addSubtaskDivFormDivInput)
+                        
+                        const addSubtaskDivFormDivDiv = document.createElement('div')
+                            addSubtaskDivFormDivDiv.className = 'input-group-append'
+                            addSubtaskDivFormDiv.appendChild(addSubtaskDivFormDivDiv)
+            
+                            const addSubtaskDivFormDivDivButton = document.createElement('button')
+                                addSubtaskDivFormDivDivButton.className = 'btn btn-info'
+                                addSubtaskDivFormDivDivButton.innerText = 'Add'
+                                addSubtaskDivFormDivDiv.appendChild(addSubtaskDivFormDivDivButton)
+}
+function renderSubtask(subtasksList, status, subtaskId, subtaskDescription, timeSpent) {
+    const subtaskLi = document.createElement('li')
+        subtaskLi.className = 'list-group-item d-flex justify-content-between align-items-center'
+        subtasksList.appendChild(subtaskLi)
+  
+        const subtaskLeftDiv = document.createElement('div')
+            subtaskLeftDiv.innerText = subtaskDescription
+            subtaskLi.appendChild(subtaskLeftDiv)
+  
+            const subtaskTimeSpan = document.createElement('span')
+                subtaskTimeSpan.className = 'badge badge-success badge-pill ml-2'
+                subtaskTimeSpan.innerText = convertMinutesIntoHHMM(timeSpent)
+                subtaskLeftDiv.appendChild(subtaskTimeSpan)
+  
+        const subtaskRightDiv = document.createElement('div')
+            subtaskLi.appendChild(subtaskRightDiv)
+  
+            const plus15MinButton = document.createElement('button')
+                plus15MinButton.className = 'btn btn-outline-success btn-sm mr-2'
+                plus15MinButton.innerText = '+15m'
+                subtaskRightDiv.appendChild(plus15MinButton)
+  
+            const plus1HButton = document.createElement('button')
+                plus1HButton.className = 'btn btn-outline-success btn-sm mr-2'
+                plus1HButton.innerText = '+1h'
+                subtaskRightDiv.appendChild(plus1HButton)
+  
+            const deleteSubtaskButton = document.createElement('button')
+                deleteSubtaskButton.className = 'btn btn-outline-danger btn-sm'
+                deleteSubtaskButton.innerText = 'Delete'
+                subtaskRightDiv.appendChild(deleteSubtaskButton)
+}
 
 
 document.addEventListener('DOMContentLoaded', function() {
